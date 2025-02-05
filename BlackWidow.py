0af8d1c8177e6f7aef61ebcb23e71211ca6bbdb6
Hayven Baarson <hcbaarson@outlook.com>
2025-02-05 14:15:32 -0500

    Added Weather (Function) - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,16 @@
-# Programmer: Hayven Baarson
-# Branch: Weather
-# Date:
-# Program:
+print("\n**********************************\n")
+
+print("Weather Branch - Developer: Hayven Baarson\n")
+
+#Import Libraries HERE!
+import random
+from time import sleep
+
+# Weather Function to determine the weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard" ,"ice" ,"rainy" ,"windy" ,"sunny"]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+    
+print(weather())
+
