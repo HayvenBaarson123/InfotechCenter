05e5c9861da1cf08030f90a324b206aa93925b25
Hayven Baarson <hcbaarson@outlook.com>
2025-02-11 14:00:54 -0500

    made the code more efficient using Chatgpt

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,67 +1,19 @@
-# Print initial message with a separator
-print("\n**********************************\n")
+import random
 
-# Print developer information
-print("Weather Branch - Developer: Hayven Baarson")
+# Weather conditions with alarm time and speed
+weather_responses = {
+    "snowing": (30, 60), "blizzard": (60, 50),
+    "rainy": (10, 70), "windy": (60, 75), "icy": (60, 55)
+}
 
-# Import required libraries
-import random  # For generating random weather condition
-from time import sleep  # For adding delays if needed (though not used in this code)
+weatherAlert = random.choice(list(weather_responses.keys()))
+alarm_time, speed = weather_responses.get(weatherAlert, (None, None))
 
-# Weather function to determine the weather condition
-def weather():
-    # List of possible weather conditions
-    weatherForecastList = ["snowing", "blizzard" ,"icy" ,"rainy" ,"windy" ,"sunny"]
-
-    # Randomly choose one weather condition from the list
-    weatherCondition = random.choice(weatherForecastList)
-
-    # Return the chosen weather condition
-    return weatherCondition
-
-# Call the weather function and store the result in the variable 'weatherAlert'
-weatherAlert = weather()
-
-# Vehicle response system based on weather conditions
-def vehicleResponseSystem():
-    # Check if the weather is snowing
-    if weatherAlert == "snowing":
-        # Update alarm time based on snowing weather
-        print("\nThe National Weather Service has updated your alarm by 30 minutes because"
-              " it is", weatherAlert, "outside.")
-        sleep(1)
-        ("VRS has been initiated allowing us to go 60MPH")
-    # Check if the weather is a blizzard
-    elif weatherAlert == "blizzard":
-        # Update alarm time for blizzard conditions
-        print("\nThe National Weather Service has updated your alarm by 60 minutes because"
-              " it is a", weatherAlert, "outside")
-        sleep(1)
-        print("VRS has been initiated allowing us to go 50MPH")
-    # Check if the weather is rainy
-    elif weatherAlert == "rainy":
-        # Update alarm time for rainy conditions
-        print("\nThe National Weather Service has updated your alarm by 10 minutes because"
-              " it is a", weatherAlert, "outside")
-        sleep(1)
-        print("VRS has been initiated allowing us to go 70MPH")
-    # Check if the weather is windy
-    elif weatherAlert == "windy":
-        # Update alarm time for windy conditions
-        print("\nThe National Weather Service has updated your alarm by 60 minutes because"
-              " it is a", weatherAlert, "outside")
-        sleep(1)
-        print("VRS has been initiated allowing us to go 75MPH")
-    # Check if the weather is icy
-    elif weatherAlert == "icy":
-        # Update alarm time for icy conditions
-        print("\nThe National Weather Service has updated your alarm by 60 minutes because"
-              " it is a", weatherAlert, "outside")
-        sleep(1)
-        print("VRS has been initiated allowing us to go 55MPH")
-    else:
-        # If the weather is sunny, notify the user to drive safe
-        print("\nThe National Weather Service is calling for", weatherAlert, "sky outside, drive safe!")
-
-# Call the vehicleResponseSystem function to execute the weather alert system
-vehicleResponseSystem()
+# Output based on weather condition
+print(f"\n**********************************\nWeather Branch - Developer: Hayven Baarson")
+if alarm_time:
+    print(f"\nThe National Weather Service has updated your alarm by {alarm_time} minutes because it is {weatherAlert} outside.")
+    print(f"VRS has been initiated allowing us to go {speed} MPH.")
+else:
+    print(f"\nThe National Weather Service is calling for {weatherAlert} sky outside, drive safe!")
+    print("\nVRS has been Deactivated allowing us to go however fast you want.")
