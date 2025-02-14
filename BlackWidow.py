38c451629165f1d817b57afca57a500b82a7f5a7
Hayven Baarson <hcbaarson@outlook.com>
2025-02-14 13:53:20 -0500

    added libraries and gas station () - NOT TESTED - Programmed in GitHub

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,2 +1,10 @@
 print("\n******************************************************\n")
 print("Gasoline Branch - Developer: Hayven Baarson")
+
+import random
+from time import sleep
+
+def gasLevelGauge():
+  gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+  return random.choice(gasLevelList)
+  
