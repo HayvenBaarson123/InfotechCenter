b1b3a2675479b4b48545d6c2d82363a2b6039300
Hayven Baarson <hcbaarson@outlook.com>
2025-02-14 14:06:26 -0500

    Added gasStations() - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -7,4 +7,10 @@ from time import sleep
 def gasLevelGauge():
   gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
   return random.choice(gasLevelList)
-  
+
+def gasStation():
+  gasStationsList = ["Shell","Marathon","Speedway","Circle K","7/11","Wesco","Meijer","WaWa"]
+  return random.choice(gasStationsList)
+
+print(gasLevelGauge())
+print(gasStation())
