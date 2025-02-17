4cc7bcab80b336fd23b957c96943a7a87e4f14b9
HayvenBaarson123 <hcbaarson@outlook.com>
2025-02-17 13:30:14 -0500

    fixed gasoline branch and main branch

--- BlackWidow.py
+++ BlackWidow.py
@@ -7,6 +7,7 @@ from time import sleep
 def gasLevelGauge():
   gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
   return random.choice(gasLevelList)
+  
 
 def gasStation():
   gasStationsList = ["Shell","Marathon","Speedway","Circle K","7/11","Wesco","Meijer","WaWa"]
